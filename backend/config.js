800cfbf4687ed9794e4b2f002139bcba15d6ce87
SDHRSN2003 <svsudharsan2003@gmail.com>
2024-06-03 16:18:40 +0530

    Application connected with database and assigned with port

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const port = 3333;
+
+export const mongodburl = "mongodb://localhost:27017/AirplaneService";
---
+++ backend/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import {port, mongodburl} from "./config.js";
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.get('/',(request,response)=>{
+    console.log(request);
+    return response.status(234).send("checking status");
+})
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+})
+
+mongoose.connect(mongodburl).then(()=>{
+    console.log("Connected to database");
+}).catch((error)=>{
+    console.log("Error in connection");
+})
